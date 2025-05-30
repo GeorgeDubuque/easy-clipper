2e4ffeafd6bd00583477965577ebf049d8b3c0db
gtdubuque <gtdubuque@gmail.com>
2025-05-30 09:11:23 -0700

    realized i cant use nvfbc cause im using wayland currently; so just connected to wayland client for now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use wayland_client::Connection;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    Connection::connect_to_env().unwrap();
+    println!("connected to wayland client!");
+    Ok(())
 }
